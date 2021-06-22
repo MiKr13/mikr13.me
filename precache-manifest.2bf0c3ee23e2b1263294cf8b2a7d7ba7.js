ce9bb6c93f389c3d6804fdc69175eba1a694b5c1
Mihir Kumar <mihirpandey.13@gmail.com>
2021-06-23 01:46:45 +0530

    Updates

---
+++ precache-manifest.2bf0c3ee23e2b1263294cf8b2a7d7ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83206435b193032b51b",
+    "url": "/css/app.3f59b3bc.css"
+  },
+  {
+    "revision": "8dbaeac4cf035e9fbbdbc8d4fe911813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c83206435b193032b51b",
+    "url": "/js/app.53ba2265.js"
+  },
+  {
+    "revision": "4e2534afcc5c1d42d749",
+    "url": "/js/chunk-vendors.b2442487.js"
+  },
+  {
+    "revision": "ef238d29398756a3eb2bfdeef182e0f1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
